1044a5dc44a72f426182666a4ae4be313204f920
olivier <olivier@alpha2real.com>
2012-12-20 22:35:10 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpatialSearch::Application.config.secret_token = '7cc65101c932b0e1381d8af30da7d72d3226e284afb5586124a230ba1ce13138fa4a777ad7561f1cef963e785215bab9ec68310c91d825ad6056bf2f0de659e2'
